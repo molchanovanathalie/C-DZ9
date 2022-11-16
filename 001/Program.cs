fbe8a3514fc9dcc3e209d343d2f54f3b7fa3039f
Natalya Molchanova <molchanova.nathalie@yandex.ru>
2022-11-16 22:07:54 +0300

    add 001

--- 001/Program.cs
+++ 001/Program.cs
@@ -1,1 +1,8 @@
 ﻿// Показать натуральные числа от M до N, N и M заданы
+void RecurNM(int i, int j)
+{
+    if (j < i) return;
+    RecurNM(i, j - 1);
+    Console.Write(j + " ");
+}
+RecurNM(3, 11);
