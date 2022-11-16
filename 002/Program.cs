d5042b76d7869ca5853bb1d4380e296f5f6d65a6
Natalya Molchanova <molchanova.nathalie@yandex.ru>
2022-11-16 22:19:01 +0300

    add 002

--- 002/Program.cs
+++ 002/Program.cs
@@ -1,1 +1,7 @@
 ﻿// Найти сумму элементов от M до N, N и M заданы
+int SumElements(int i, int j)
+{
+    if (i == j) return i;
+    return i + SumElements(i + 1, j);
+}
+Console.WriteLine(SumElements(1, 8));
