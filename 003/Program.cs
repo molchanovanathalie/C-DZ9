fdaf3ab2e285b8d2b338e09aa02e944c5d54f844
Natalya Molchanova <molchanova.nathalie@yandex.ru>
2022-11-17 21:20:45 +0300

    add 003

---
+++ 003/Program.cs
@@ -0,0 +1,20 @@
+﻿// Написать программу вычисления функции Аккермана
+int Akkerman(int i, int j)
+{
+    if (i >6 || j > 6 || i< 0 ||j < 0) 
+    return -1;
+
+    if (i== 0)
+    {
+        return j + 1;
+    }
+    else if (j== 0)
+    {
+        return Akkerman(i- 1, 1);
+    }
+    else
+    {
+        return Akkerman(i - 1,Akkerman(i, j - 1));
+    }
+}
+Console.WriteLine(Akkerman(2, 3));
