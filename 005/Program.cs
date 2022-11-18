37f8095a2bce9c29b38d4f7987629499eb014ec6
Natalya Molchanova <molchanova.nathalie@yandex.ru>
2022-11-18 13:31:44 +0300

    add 005

---
+++ 005/Program.cs
@@ -0,0 +1,31 @@
+﻿//Написать программу показывающие первые N чисел, для которых каждое следующее равно сумме двух предыдущих.
+//Первые два элемента последовательности задаются пользователем
+int FillArray(int numberA, int numberB, int amountNumber)
+{
+    int numberC = 0;
+    if (amountNumber <= 1)
+    {
+        numberC = numberA + numberB;
+    }
+    if (amountNumber + 1 > 1)
+    {
+        numberC = numberA + numberB;
+        numberA = numberB;
+        numberB = numberC;
+        amountNumber = amountNumber - 1;
+        Console.Write($"{numberC} ");
+        FillArray(numberA, numberB, amountNumber);
+    }
+    return numberC;
+}
+
+Console.Clear();
+Console.WriteLine("Написать программу показывающую первые N чисел, для которых каждое следующее равно сумме двух предыдущих");
+Console.Write("Введите первое число : ");
+int А = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите второе число : ");
+int B = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите нужное количество дополнительных чисел : ");
+int N = int.Parse(Console.ReadLine() ?? "0");
+
+FillArray(А, B, N);
